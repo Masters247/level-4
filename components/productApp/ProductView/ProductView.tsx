ecb4fbb0cfd1e4df5a748e9bee1cc52694cd86bb
Daniel Couzens <danielcouzens@protonmail.com>
2022-03-02 15:59:28 +0000

    build error fix

--- components/productApp/ProductView/ProductView.tsx
+++ components/productApp/ProductView/ProductView.tsx
@@ -1,10 +1,6 @@
-import React, { FC, useRef, useEffect, useState } from "react";
+import React, { FC } from "react";
 import s from "./productView.module.scss";
 import ProductUi from "../ProductUi/ProductUi";
-import Gesture from "../Gesture/Gesture";
-import Resize from "../Resize-image/Resize";
-import ResizeImage from "../Resize-image/ResizeImage";
-import DragMove from "../Drag-move/DragMove";
 import Test from "../Test/Test"
 
 const ProductView: FC = () => {
