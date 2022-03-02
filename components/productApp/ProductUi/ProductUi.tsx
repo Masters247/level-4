341c99aa99868c43d93b84713c7b74e2e50bcd54
Daniel Couzens <danielcouzens@protonmail.com>
2022-03-02 16:33:56 +0000

    ui update

--- components/productApp/ProductUi/ProductUi.tsx
+++ components/productApp/ProductUi/ProductUi.tsx
@@ -7,24 +7,46 @@ const options = [
   { colour: "#909090" },
 ];
 
-const ProductUi: FC = () => {
+const uiButtons = [
+  { icon: "", class: s.resize, text: "resize" },
+  { icon: "", class: s.image, text: "new image" },
+  { icon: "", class: s.undo, text: "undo" },
+  { icon: "", class: s.redo, text: "redo" },
+  { icon: "", class: s.save, text: "save for letter" },
+  { icon: "", class: s.download, text: "download" },
+];
 
-    const handleColour = () => {
-        console.log("colour click")
-    }
+const ProductUi: FC = () => {
+  const handleColour = () => {
+    console.log("colour click");
+  };
   return (
     <div className={s.productUiWrap}>
-      <div className={s.colourButtonsWrap}>
+      <div className={s.productColourWrap}>
+        <h3>Product Colour:</h3>
+        <div className={s.colourButtonsWrap}>
           {options.map((colour: any, id: number) => {
-              return (
-                  <button key={id} onClick={handleColour} className={s.colourButton} style={{
-                      backgroundColor: colour.colour
-                  }}>
-                  </button>
-              )
+            return (
+              <button
+                key={id}
+                onClick={handleColour}
+                className={s.colourButton}
+                style={{
+                  backgroundColor: colour.colour,
+                }}></button>
+            );
           })}
+        </div>
       </div>
-      <p>Product Ui</p>
+      <div className={s.uiButtonsWrap}>
+        {uiButtons.map((button: any, i: any) => {
+          return (
+            <button className={`${s.uiButton} ${button.class} `}>
+              {button.text}
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 };
