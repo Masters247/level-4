55c3dc29eb91bb162a232c6640ad06c66df106b1
DanielCouzens <dan.couzens@masters247.com>
2022-03-17 16:56:36 +0000

    final changes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta
+        {/* <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0"
           id="viewportMeta"
-        />
+        /> */}
         <link rel="stylesheet" href="https://use.typekit.net/dlg6cmt.css" />
       </Head>
       <body>
