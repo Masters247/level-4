54c26a061eafa5dcfaeba3766056ccef9449e570
Daniel Couzens <danielcouzens@protonmail.com>
2022-06-16 17:01:39 +0100

    not quite fixed

--- components/productVisualiserApp/ImageUploader/ImageLogo.tsx
+++ components/productVisualiserApp/ImageUploader/ImageLogo.tsx
@@ -14,37 +14,53 @@ const ImageLogo = ({
   selectImage,
   onImageLocalRemove,
 }: any) => {
+  const [isSelected, setIsSelected] = useState(false);
+  const selectedLogo = window.localStorage.getItem("selected-logo");
+
+  // useEffect(() => {
+  //   setSelectImage(selectedLogo);
+  //   setIsSelected(true);
+  // }, [isSelected]);
+
+  // console.log("file: ImageLogo.tsx ~ line 20 ~ selectedLogo", selectedLogo);
+  // let locallyStoredLogos: any = window.localStorage.getItem("logo list");
+
   const handleSelected = (index: any) => {
+    setIsSelected(true);
+
     setSelectImage(index);
+
+    // setIsSelected(true);
+    // setSelectImage(index);
+    window.localStorage.setItem("selected-logo", index);
   };
 
   return (
     <div
-      className={cn(s.imageItem, selectImage === index && s.picked)}
-      onClick={() => handleSelected(index)}
-    >
+      className={cn(
+        s.imageItem,
+        selectImage === index && isSelected && s.picked
+      )}
+      onClick={() => handleSelected(index)}>
       <button
         type="button"
         onClick={() => handleLogoPick(imageList, index)}
         className={s.handleLogoPickButton}
-        style={{ height: "100%", width: "100%" }}
-      >
+        style={{ height: "100%", width: "100%" }}>
         <img src={image.data_url} alt="uploaded logo" />
       </button>
       {isLocal ? (
         <button
           className={s.imageCloseBtn}
           type="button"
-          onClick={() => onImageLocalRemove(index)}
-        >
+          onClick={() => onImageLocalRemove(index)}>
           <Remove styles={s.remove} />
         </button>
       ) : (
         <button
           className={s.imageCloseBtn}
           type="button"
-          onClick={() => onImageRemove(index)}
-        >
+          onClick={() => onImageRemove(index)}>
           <Remove styles={s.remove} />
         </button>
       )}
